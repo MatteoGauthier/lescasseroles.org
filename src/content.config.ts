ea2ff88ce45ca8c63a2affd477749f832e945808
Mattèo Gauthier <matteo.gauthier@gmail.com>
2025-02-24 13:22:16 +0100

    feat: improve the sources and styling

--- src/content.config.ts
+++ src/content.config.ts
@@ -53,21 +53,32 @@ const controversy = defineCollection({
         url: z.string().url(),
         publishedAt: z.coerce.date(),
         shortName: z.string().optional(),
-        source: reference("source").optional(),
+        source: z.array(reference("source")).optional(),
       })
     ),
     categories: z.array(reference("category")),
   }),
 })
 
+const sourceTagSlugs = ["independant"] as const
+
+const sourceTag = defineCollection({
+  loader: file("content/source-tags.json"),
+  schema: z.object({
+    name: z.string(),
+    slug: z.string(),
+  }),
+})
+
 const source = defineCollection({
   loader: file("content/sources.json"),
   schema: z.object({
-    title: z.string(),
+    name: z.string(),
     description: z.string().optional(),
     slug: z.string(),
-
     website: z.string().url(),
+    tags: z.array(reference("sourceTag")),
+    links: z.array(z.string().url()),
   }),
 })
 
@@ -75,4 +86,6 @@ export const collections = {
   entity,
   controversy,
   category,
+  source,
+  sourceTag,
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in": {
+          '0%': { opacity: '0%' },
+          '100%': { opacity: '100%' },
+        }
+      },
+      animation: {
+        "fade-in": 'fade-in 0.5s ease-in-out',
+      } 
+    }
   },
   plugins: [require("@tailwindcss/typography")],
 }
